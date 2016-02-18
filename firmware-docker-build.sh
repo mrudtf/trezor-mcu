af6041fbdd683f9db89c68985f5690643b857a4b
karek314 <karek314@protonmail.ch>
2016-02-19 00:52:50 +0100

    Update firmware-docker-build.sh
    use my develop branch

--- firmware-docker-build.sh
+++ firmware-docker-build.sh
@@ -4,7 +4,7 @@ FIRMWARETAG=${1:-master}
 
 docker build -t $IMAGETAG .
 docker run -t -v $(pwd)/output:/output $IMAGETAG /bin/sh -c "\
-	git clone https://github.com/trezor/trezor-mcu && \
+	git clone https://github.com/karek314/trezor-mcu && \
 	cd trezor-mcu && \
 	git checkout $FIRMWARETAG && \
 	git submodule update --init && \
